0b19f8055aede8a3e502346feeff8e325e643788
Sudhanshu Shevade <sudhanshu.shevade@gmail.com>
2019-01-30 22:18:04 -0500

    Updates

---
+++ precache-manifest.56995c1bd5ae376c178a3abb95917d1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "75dbdbdf74143e8f81ca",
+    "url": "/weathercheck/static/js/runtime~main.75dbdbdf.js"
+  },
+  {
+    "revision": "7a87f3c1a21e8a04b953",
+    "url": "/weathercheck/static/js/main.7a87f3c1.chunk.js"
+  },
+  {
+    "revision": "b92b7318af9f12137c47",
+    "url": "/weathercheck/static/js/1.b92b7318.chunk.js"
+  },
+  {
+    "revision": "7a87f3c1a21e8a04b953",
+    "url": "/weathercheck/static/css/main.99fa0ee6.chunk.css"
+  },
+  {
+    "revision": "b92b7318af9f12137c47",
+    "url": "/weathercheck/static/css/1.415c98d9.chunk.css"
+  },
+  {
+    "revision": "ee681ac6bb14056ccb746ed5e30e9a97",
+    "url": "/weathercheck/index.html"
+  }
+];
